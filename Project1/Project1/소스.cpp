c6ea9a903d52c8616dabd5fde025579ebced6150
akfakf0509 <50164284+akfakf0509@users.noreply.github.com>
2019-09-23 12:39:50 +0900

    2019.09.23 12.39

--- Project1/Project1/소스.cpp
+++ Project1/Project1/소스.cpp
@@ -10,7 +10,7 @@ int main() {
 	cin >> input;
 
 	for (unsigned int a = 0; a < input.length(); a++) {
-		if(input[a] >= 'a')
+		if (input[a] >= 'a')
 			input[a] -= 'a' - 'A';
 
 		if (!(input[a] == 'A' || input[a] == 'T' || input[a] == 'G' || input[a] == 'C')) {
@@ -18,49 +18,93 @@ int main() {
 
 			return 0;
 		}
-			
-		if (input[a] == 'T') {
+
+		if (input[a] == 'A') {
 			input[a] = 'U';
+		}
+		if (input[a] == 'T') {
+			input[a] = 'A';
+		}
+		if (input[a] == 'C') {
+			input[a] = 'G';
+		}
+		if (input[a] == 'G') {
+			input[a] = 'C';
 		}
 	}
 
-	cout << "-=-=-=-=-=-=-=-=-=-=-=-=-" << endl << "코돈 : ";
+	cout << "-=-=-=-=-=-=-=-=-=-=-=-=-" << endl;
 
 	for (unsigned int a = 0; a < input.length(); a += 3) {
+		if (a != 0)
+			cout << "-";
+		cout << (char)input[a] << (char)input[a + 1] << (char)input[a + 2];
+	}
+
+	cout << endl;
+
+	for (unsigned int a = 0; a < input.length(); a += 3) {
+		if (a != 0)
+			cout << " | ";
+
 		switch (input[a]) {
 		case 'U':
+
 			switch (input[a + 1]) {
 			case 'U':
+
 				switch (input[a + 2]) {
 				case 'U':
 				case 'C':
 					cout << "페닐 알라닌";
 					break;
-				case'A':
-				case'G':
+				case 'A':
+				case 'G':
 					cout << "류신";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
 					break;
 				}
 				break;
+
 			case 'C':
-				cout << "세린";
-				break;
-			case 'A':
+
 				switch (input[a + 2]) {
 				case 'U':
-				case'C':
+				case 'C':
+				case 'A':
+				case 'G':
+					cout << "세린";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
+				}
+				break;
+
+			case 'A':
+
+				switch (input[a + 2]) {
+				case 'U':
+				case 'C':
 					cout << "타이로신";
 					break;
 				case 'A':
-				case'G':
+				case 'G':
 					cout << "종결 코돈";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
 					break;
 				}
 				break;
+
 			case 'G':
+
 				switch (input[a + 2]) {
 				case 'U':
-				case'C':
+				case 'C':
 					cout << "시스테인";
 					break;
 				case 'A':
@@ -69,101 +113,234 @@ int main() {
 				case 'G':
 					cout << "트립토판";
 					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
 				}
+				break;
+
+			default:
+				cout << "해당하는 아미노산이 없습니다.";
 				break;
 			}
 			break;
+
 		case 'C':
+
 			switch (input[a + 1]) {
 			case 'U':
-				cout << "류신";
+
+				switch (input[a + 2]) {
+				case 'U':
+				case 'C':
+				case 'A':
+				case 'G':
+					cout << "류신";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
+				}
 				break;
-			case'C':
-				cout << "프롤린";
+
+			case 'C':
+
+				switch (input[a + 2]) {
+				case 'U':
+				case 'C':
+				case 'A':
+				case 'G':
+					cout << "프롤린";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
+				}
 				break;
+
+			case 'A':
+
+				switch (input[a + 2]) {
+				case 'U':
+				case 'C':
+					cout << "히스티딘";
+					break;
+				case 'A':
+				case 'G':
+					cout << "글루타민";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
+				}
+				break;
+
+			case 'G':
+
+				switch (input[a + 2]) {
+				case 'U':
+				case 'C':
+				case 'A':
+				case 'G':
+					cout << "아르지닌";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
+				}
+				break;
+
+			default:
+				cout << "해당하는 아미노산이 없습니다.";
+				break;
+			}
+			break;
+		case 'A':
+
+			switch (input[a + 1]) {
+			case 'U':
+
+				switch (input[a + 2]) {
+				case 'U':
+				case 'C':
+				case 'A':
+					cout << "아이 소류신";
+					break;
+				case 'G':
+					cout << "시작 코돈";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
+				}
+				break;
+
+			case 'C':
+				switch (input[a + 2]) {
+				case 'U':
+				case 'C':
+				case 'A':
+				case 'G':
+					cout << "트레오닌";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
+				}
+				break;
+
 			case 'A':
 				switch (input[a + 2]) {
 				case 'U':
-				case'C':
-					cout << "히스티딘";
-					break;
-				case'A':
-				case'G':
-					cout << "글루타민";
-					break;
-				}
-				break;
-			case'G':
-				cout << "아르지닌";
-				break;
-			}
-			break;
-		case'A':
-			switch (input[a + 1]) {
-			case'U':
-				switch (input[a + 2]) {
-				case'U':
-				case'C':
-				case'A':
-					cout << "아이 소류신";
-					break;
-				case'G':
-					cout << "시작 코돈";
-					break;
-				}
-				break;
-			case 'C':
-				cout << "트레오닌";
-				break;
-			case 'A':
-				switch (input[a + 2]) {
-				case'U':
-				case'C':
+				case 'C':
 					cout << "아스파라긴";
 					break;
-				case'A':
-				case'G':
+				case 'A':
+				case 'G':
 					cout << "라이신";
 					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
 				}
 				break;
-			case'G':
+
+			case 'G':
 				switch (input[a + 2]) {
-				case'U':
-				case'C':
+				case 'U':
+				case 'C':
 					cout << "세린";
 					break;
-				case'A':
-				case'G':
+				case 'A':
+				case 'G':
 					cout << "아르지닌";
 					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
 				}
+				break;
+
+			default:
+				cout << "해당하는 아미노산이 없습니다.";
 				break;
 			}
 			break;
-		case'G':
+
+		case 'G':
+
 			switch (input[a + 1]) {
-			case'U':
-				cout << "발린";
-				break;
-			case'C':
-				cout << "알라닌";
-				break;
-			case'A':
+			case 'U':
 				switch (input[a + 2]) {
-				case'U':
-				case'C':
+				case 'U':
+				case 'C':
+				case 'A':
+				case 'G':
+					cout << "발린";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
+				}
+				break;
+
+			case 'C':
+				switch (input[a + 2]) {
+				case 'U':
+				case 'C':
+				case 'A':
+				case 'G':
+					cout << "알라닌";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
+				}
+				break;
+
+			case 'A':
+				switch (input[a + 2]) {
+				case 'U':
+				case 'C':
 					cout << "아스파트산";
 					break;
-				case'A':
-				case'G':
+				case 'A':
+				case 'G':
 					cout << "글루탐산";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
 					break;
 				}
 				break;
+
+			case 'G':
+				switch (input[a + 2]) {
+				case 'U':
+				case 'C':
+				case 'A':
+				case 'G':
+					cout << "글라이신";
+					break;
+				default:
+					cout << "해당하는 아미노산이 없습니다.";
+					break;
+				}
+				break;
+
+			default:
+				cout << "해당하는 아미노산이 없습니다.";
+				break;
 			}
 			break;
+
+		default:
+			cout << "해당하는 아미노산이 없습니다.";
+			break;
 		}
-		cout << " | ";
 	}
+
+	cout << endl;
+
+	system("pause");
 }
